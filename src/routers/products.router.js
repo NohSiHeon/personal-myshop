19b5838323d72f622d17622e411a767bfeb78ece
NohSiHeon <shtlgjs016@naver.com>
2024-05-16 21:59:30 +0900

    상품 목록 조회 API 완성

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -4,33 +4,40 @@ import Product from "../schemas/product.schema.js";
 const router = express.Router();
 
 /* 상품 등록 API */
-router.post("/product", async (req, res) => {
-    const { name, description, manager, password } = req.body;
-	
-	
+router.post("/products", async (req, res) => {
+	const { name, description, manager, password } = req.body;
 
-    const createdProduct = await Product.create({
-        name,
-        description,
-        manager,
-		password,
+	const product = new Product({
+		name,
+		description,
+		manager,
+		password
+	});
+	await product.save();
 
-    });
 
-	const product = new Product({ createdProduct });
-	product.save();
-
-    return res.status(201).json({ product });
+	return res.status(201).json({
+		"status": 201,
+		"message": "상품 생성에 성공했습니다.",
+		"data": product
+	});
 });
 
 /* 상품 목록 조회 API */
-router.get("/product", async (req, res) => {
+router.get("/products", async (req, res) => {
 	const products = await Product.find().sort('-createdAt').exec();
 
-    return res.status(200).json({products})
+	return res.status(201).json({
+		"status": 201,
+		"message": "상품 생성에 성공했습니다.",
+		"data": products
+	})
 });
 
 /* 상품 상세 조회 API */
+// router.get("/products/:id", async(req, res)=> {
+
+// })
 /* 상품 수정 API */
 /* 상품 삭제 API */
 
--- src/schemas/product.schema.js
+++ src/schemas/product.schema.js
@@ -1,37 +1,34 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    manager: {
-        type: String,
-        required: true,
-    },
+	name: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	manager: {
+		type: String,
+		required: true,
+	},
 	password: {
 		type: String,
 		required: true,
 	},
-    condition: {
-        type: String,
-        required: true,
+	status: {
+		type: String,
 		default: "FOR_SALE",
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
 	updateAt: {
 		type: Date,
 		default: Date.now,
-	}
+	},
 });
 
 export default mongoose.model("Product", productSchema);
