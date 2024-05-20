f11fe1d6e6d44be6670fb00bd6236a3c1ec7af41
NohSiHeon <shtlgjs016@naver.com>
2024-05-20 15:13:07 +0900

    주석 추가

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -5,8 +5,10 @@ const router = express.Router();
 
 /* 상품 등록 API */
 router.post("/products", async (req, res) => {
+	// 상품 정보 파싱하기
 	const { name, description, manager, password } = req.body;
 
+	// DB에 저장하기
 	const product = new Product({
 		name,
 		description,
@@ -15,7 +17,7 @@ router.post("/products", async (req, res) => {
 	});
 	await product.save();
 
-
+	// 완료 메시지 반환하기
 	return res.status(201).json({
 		"status": 201,
 		"message": "상품 생성에 성공했습니다.",
@@ -25,32 +27,45 @@ router.post("/products", async (req, res) => {
 
 /* 상품 목록 조회 API */
 router.get("/products", async (req, res) => {
+	// DB에서 조회하기(생성일시 기준 내림차순 정렬)
 	const products = await Product.find().sort('-createdAt').exec();
 
+	// 완료 메시지 반환하기
 	return res.status(201).json({
 		"status": 201,
-		"message": "상품 생성에 성공했습니다.",
+		"message": "상품 목록 조회에 성공했습니다.",
 		"data": products
 	})
 });
 
 /* 상품 상세 조회 API */
 router.get("/products/:id", async (req, res) => {
+	// 상품 ID 파싱하기
 	const { id } = req.params;
+
+	// DB에서 조회하기
 	const products = await Product.findById(id).select('name description manager status createdAt updateAt').exec();
 
+	// 완료 메시지 반환하기
 	return res.status(200).json({
 		"status": 200,
 		"message": "상품 상세 조회에 성공했습니다.",
 		"data": products
 	})
 })
+
 /* 상품 수정 API */
 router.patch('/products/:id', async (req, res) => {
+	// 상품 ID 파싱하기
 	const { id } = req.params;
+
+	// 상품 수정 정보 파싱하기
 	const { name, description, manager, status, password } = req.body;
 
+	// DB에서 조회하기(패스워드 포함)
 	const products = await Product.findById(id).exec();
+
+	// 비밀번호 일치 여부 확인
 	if (products.password !== password) {
 		return res.status(401).json({ errorMessage: '비밀번호가 일치하지 않습니다.' })
 	} else {
@@ -59,32 +74,40 @@ router.patch('/products/:id', async (req, res) => {
 		if (manager !== null) products.manager = manager;
 		if (status !== null) products.status = status;
 	}
-
+	// DB에 갱신하기
 	await products.save();
 
+	// 완료 메시지 반환하기
 	return res.status(200).json({ products });
 
 })
 
 /* 상품 삭제 API */
 router.delete("/products/:id", async (req, res) => {
+
+	// 상품 ID 반환하기
 	const { id, } = req.params;
+
+	// 패스워드 파싱하기
 	const { password } = req.body;
 
+	// DB에서 조회하기(패스워드 포함)
 	const products = await Product.findById(id).exec();
 	const productId = await Product.findById(id).select('id').exec();
+
+	// 비밀번호 일치 여부 확인
 	if (products.password !== password) {
 		return res.status(401).json({ errorMessage: '비밀번호가 일치하지 않습니다.' });
 	}
+	// DB에서 삭제하기
 	await Product.deleteOne({ _id: id }).exec();
 
-
+	// 완료 메시지 반환하기
 	return res.status(200).json({
 		"status": 200,
 		"message": "상품 삭제에 성공했습니다.",
 		"data": productId,
 	})
 })
-
 
 export default router;
